05f1483e389d0cc033713101a43292c165546270
Narcis Simu <narcis.simu@gmail.com>
2018-07-26 12:18:22 +0200

    Add Links

--- scripts/jiratasks.js
+++ scripts/jiratasks.js
@@ -9,23 +9,19 @@ define(function () {
           "integrated": "",
           "platform": "ePages6",
           "relevance": "minor",
-          "links": [
-
-          ]
+          "links": []
       },
       {
-        "id": "AD-8504",
-        "name": "DHL: (re)activate ESF Test eP6",
-        "type": "Task",
-        "summary": "",
-        "priority": "Major",
-        "integrated": "7.27.0",
-        "platform": "ePages6",
-        "relevance": "major",
-        "links": [
-            "<a href='img/dhl_esf_results.jpg' target='_blank'>DHL ESF results</a>"
-        ]
-    },
+          "id": "AD-8880",
+          "name": "Research: Amazon Pay: new SDK implementation",
+          "type": "Research",
+          "summary": "",
+          "priority": "Major",
+          "integrated": "",
+          "platform": "ePages6",
+          "relevance": "minor",
+          "links": []
+      },
     {
         "id": "AD-8730",
         "name": "Google Shopping: Use correct product URLs for feedfile",
@@ -78,6 +74,10 @@ define(function () {
         "relevance": "major",
         "links": [
             "<a href='http://easy.vm-intern.epages.com:9000/component_measures/metric/coverage/history?id=DE_EPAGES%3A%3ASpreedly' target='_blank'>Coverage</a>",
+            "<a href='https://us-unity.teamred-06.vm-intern.epages.com/epages/us_unity.admin/secdb80e244f5/?ViewAction=UnityMBO-ViewPaymentList&ObjectID=28580' target='_blank'>ePagesNow MBO</a>",
+            "<a href='https://us-unity.teamred-06.vm-intern.epages.com/' target='_blank'>ePagesNow SF</a>",
+            "<a href='https://teamred-06.vm-intern.epages.com/epages/DemoShop.admin/sec355f4bb918/?ViewAction=MBO-ViewPaymentList&ObjectID=20897' target='_blank'>ePages6 MBO</a>",
+            "<a href='http://teamred-06.vm-intern.epages.com/epages/DemoShop.sf' target='_blank'>ePages6 SF</a>",
         ]
     },
     {
@@ -91,8 +91,24 @@ define(function () {
         "relevance": "major",
         "links": [
             "<a href='http://easy.vm-intern.epages.com:9000/component_measures/metric/coverage/history?id=DE_EPAGES%3A%3AMoneybookers' target='_blank'>Coverage</a>",
-
+            "<a href='https://us-unity.teamred-06.vm-intern.epages.com/epages/us_unity.admin/secdb80e244f5/?ViewAction=UnityMBO-ViewPaymentList&ObjectID=28580' target='_blank'>ePagesNow MBO</a>",
+            "<a href='https://us-unity.teamred-06.vm-intern.epages.com/' target='_blank'>ePagesNow SF</a>",
+            "<a href='https://teamred-06.vm-intern.epages.com/epages/DemoShop.admin/sec355f4bb918/?ViewAction=MBO-ViewPaymentList&ObjectID=20897' target='_blank'>ePages6 MBO</a>",
+            "<a href='http://teamred-06.vm-intern.epages.com/epages/DemoShop.sf' target='_blank'>ePages6 SF</a>",
         ]
-    }
+    },
+    {
+      "id": "AD-8504",
+      "name": "DHL: (re)activate ESF Test eP6",
+      "type": "Task",
+      "summary": "",
+      "priority": "Major",
+      "integrated": "7.27.0",
+      "platform": "ePages6",
+      "relevance": "major",
+      "links": [
+          "<a href='img/dhl_esf_results.jpg' target='_blank'>DHL ESF results</a>"
+      ]
+  }
 ]
 })
